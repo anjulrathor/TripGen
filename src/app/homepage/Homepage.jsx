cdd9239560313dacc3f42bb57ce6e0ace9c6d271
Anjul <rathor.anjul124@gmail.com>
2025-11-20 12:24:20 +0530

    landing page

---
+++ src/app/homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroSection from "../components/layout/home/HeroSection";
+
+const HomePage = () => {
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
+};
+
+export default HomePage;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import HomePage from './homepage/Homepage'
 
 const page = () => {
   return (
     <div>
-      
+      <HomePage/>
     </div>
   )
 }
